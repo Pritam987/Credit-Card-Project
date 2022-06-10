408e869348437b63c4094457787ebbd18972668e
Pritam <pritamsaha075@gmail.com>
2022-06-10 14:36:03 +0530

    up22

--- r.R
+++ r.R
@@ -1,3 +1,8 @@
+#Before PCA
 library(NbClust)
 dataset <- read.csv('C:\\Users\\Pritam\\Desktop\\Project 4th sem\\Data_processed.csv')
+NbClust(data=dataset,method ="kmeans")
+#After PCA
+library(NbClust)
+dataset <- read.csv('C:\\Users\\Pritam\\Desktop\\Project 4th sem\\Data_processed_after_pca.csv')
 NbClust(data=dataset,method ="kmeans")
--- r_after_pca.R
+++
@@ -1,3 +0,0 @@
-library(NbClust)
-dataset <- read.csv('C:\\Users\\Pritam\\Desktop\\Project 4th sem\\Data_processed_after_pca.csv')
-NbClust(data=dataset,method ="kmeans")
