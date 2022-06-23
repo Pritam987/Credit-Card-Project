7011b89d6187b9e61a52f2b69fc78758046de2ee
Pritam <pritamsaha075@gmail.com>
2022-06-23 13:06:33 +0530

    upd11

--- r.R
+++ r.R
@@ -11,3 +11,22 @@ library(mclust)
 dataset <- read.csv('C:\\Users\\Pritam\\Desktop\\Project 4th sem\\Data_processed.csv')
 model=Mclust(dataset)
 summary(model) 
+
+#Before PCA - Validation Methods
+#v1
+library(clValid)
+library(mclust)
+dataset<- read.csv('C:\\Users\\Pritam\\Desktop\\Project 4th sem\\Data_processed.csv')
+rownames(dataset)<-1:nrow(dataset)
+summary(clValid(dataset,5, clMethods=c("hierarchical","kmeans","pam","model"),
+                  validation="internal"))
+#v2
+library(clValid)
+dataset<- read.csv('C:\\Users\\Pritam\\Desktop\\Project 4th sem\\Data_processed.csv')
+rownames(dataset)<-1:nrow(dataset)
+summary(clValid(dataset,5,clMethods =c("diana", "fanny"),validation="internal"))
+#v3
+library(clValid)
+dataset<- read.csv('C:\\Users\\Pritam\\Desktop\\Project 4th sem\\Data_processed.csv')
+rownames(dataset)<-1:nrow(dataset)
+summary(clValid(dataset,5, clMethods=c("som","agnes","clara"),validation="internal"))
