5a4288bcbb2412c39f33f7ce76844746207a1108
Pritam <pritamsaha0123456789@gmail.com>
2022-07-17 22:07:56 +0530

    u12

--- r.R
+++ r.R
@@ -1,6 +1,6 @@
 library(NbClust)
 dataset <- read.csv('C:\\Users\\Pritam\\Desktop\\Project 4th sem\\Data_processed.csv')
-NbClust(data=dataset,method ="kmeans")
+NbClust(data=dataset,method ="kmeans",distance="euclidean",min.nc=2,max.nc=8)
 
 library(mclust)
 dataset <- read.csv('C:\\Users\\Pritam\\Desktop\\Project 4th sem\\Data_processed.csv')
