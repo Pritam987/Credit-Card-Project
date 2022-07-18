2c2d8f5ea274450f62fd6de80b51c3e3b3a010d6
Pritam <pritamsaha0123456789@gmail.com>
2022-07-18 16:44:44 +0530

    u0

--- r.R
+++ r.R
@@ -6,3 +6,8 @@ library(mclust)
 dataset <- read.csv('C:\\Users\\Pritam\\Desktop\\Project 4th sem\\Data_processed.csv')
 model=Mclust(dataset)
 summary(model) 
+
+library(NbClust)
+dataset <- read.csv('C:\\Users\\Pritam\\Desktop\\Project 4th sem\\Data.csv')
+NbClust(na.omit(dataset[,2:18]),method ="kmeans",distance="euclidean",min.nc=2,max.nc=8,index="all")
+
