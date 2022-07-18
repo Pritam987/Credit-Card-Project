a87cbd80817826d1bbc0f30cf85cd7215c844959
Pritam <pritamsaha0123456789@gmail.com>
2022-07-18 19:44:17 +0530

    p2

--- r.R
+++ r.R
@@ -1,7 +1,6 @@
 dataset <- read.csv('C:\\Users\\Pritam\\Desktop\\Project 4th sem\\Data.csv')
 data=na.omit(dataset[,2:18])
-library("writexl")
-write_xlsx(data,'C:\\Users\\Pritam\\Desktop\\Project 4th sem\\Data_processed.csv')
+write.csv(data,'C:\\Users\\Pritam\\Desktop\\Project 4th sem\\Data_processed.csv',row.names=F)
 
 library(NbClust)
 NbClust(data,method ="kmeans",distance="euclidean",min.nc=2,max.nc=8,index="all")
