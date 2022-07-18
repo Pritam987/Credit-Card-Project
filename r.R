1ba0082c3b7debdf1fe3f590c2cc9b5bb5ffbd7b
Pritam <pritamsaha0123456789@gmail.com>
2022-07-18 19:15:20 +0530

    p1

--- r.R
+++ r.R
@@ -1,13 +1,11 @@
+dataset <- read.csv('C:\\Users\\Pritam\\Desktop\\Project 4th sem\\Data.csv')
+data=na.omit(dataset[,2:18])
+library("writexl")
+write_xlsx(data,'C:\\Users\\Pritam\\Desktop\\Project 4th sem\\Data_processed.csv')
+
 library(NbClust)
-dataset <- read.csv('C:\\Users\\Pritam\\Desktop\\Project 4th sem\\Data_processed.csv')
-NbClust(data=dataset,method ="kmeans",distance="euclidean",min.nc=2,max.nc=8,index="all")
+NbClust(data,method ="kmeans",distance="euclidean",min.nc=2,max.nc=8,index="all")
 
 library(mclust)
-dataset <- read.csv('C:\\Users\\Pritam\\Desktop\\Project 4th sem\\Data_processed.csv')
-model=Mclust(dataset)
+model=Mclust(data)
 summary(model) 
-
-library(NbClust)
-dataset <- read.csv('C:\\Users\\Pritam\\Desktop\\Project 4th sem\\Data.csv')
-NbClust(na.omit(dataset[,2:18]),method ="kmeans",distance="euclidean",min.nc=2,max.nc=8,index="all")
-
